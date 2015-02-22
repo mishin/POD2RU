e17288a5f464496964351de93aae08c2f72d296c
Nikolay Mishin <mi@ya.ru>
2015-02-23 02:30:01 +0300

    initial commit comment

---
+++ lib/POD2/RU.pm
@@ -0,0 +1,278 @@
+package POD2::RU;
+use utf8;
+use strict;
+use warnings;
+use base 'Exporter';
+use base 'POD2::Base';
+
+our $VERSION = '5.18.0.1.14';
+
+our @EXPORT = qw(print_pod print_pods);
+
+# Versions list
+sub pod_info {
+    {
+        perl        => '5.18.0.1',
+        perlintro   => '5.18.0.1',
+        perlrun     => '5.18.0.1',
+        a2p         => '5.18.0.1',
+        perlbook    => '5.18.0.1',
+        perldoc     => '5.18.0.1',
+        perlpragma  => '5.18.0.1',
+        perlstyle   => '5.18.0.1',
+        perlcheat   => '5.18.0.1',
+        perlnewmod  => '5.18.0.1',
+        perlrequick => '5.18.0.1',
+        perlreref   => '5.18.0.1',
+        perlunicode   => '5.18.0.1',
+        perlretut   => '5.18.0.1',
+        perlreguts   => '5.18.0.1',
+        perlrecharclass   => '5.18.0.1',
+        perlrebackslash   => '5.18.0.1',
+        perlreapi   => '5.18.0.1',
+        perlre   => '5.18.0.1',
+        perlsecret   => '5.18.0.1',
+        perlreapi   => '5.18.0.1',
+    };
+}
+
+# String for perldoc with -L switch
+sub search_perlfunc_re {
+    return 'Список функций Perl';
+}
+
+# Print information about a pod file
+sub print_pod {
+    my $self = shift;
+    prepare_encoding_console();
+    my @args = @_ ? @_ : @ARGV;
+
+    unless ( ref $self ) {
+        if ( defined $self ) {
+            if ( $self ne __PACKAGE__ ) {
+                unshift @args, $self;
+                $self = __PACKAGE__;
+            }
+        }
+        else {
+            $self = __PACKAGE__;
+        }
+    }
+
+    my $pods = $self->pod_info;
+
+    while (@args) {
+        ( my $pod = lc( shift @args ) ) =~ s/\.pod$//;
+        if ( exists $pods->{$pod} ) {
+            print
+"\t'$pod' переведены на русский Perl $pods->{$pod}\n";
+        }
+        else {
+            print "\t'$pod' еще не переведены\n";
+        }
+    }
+}
+
+# Print list of translated pods
+sub print_pods {
+    my $self = @_ ? shift : __PACKAGE__;
+    prepare_encoding_console();
+    $self->SUPER::print_pods;
+}
+
+sub prepare_encoding_console {
+    use Encode::Locale qw(decode_argv);
+    if (-t) {
+        binmode( STDIN,  ":encoding(console_in)" );
+        binmode( STDOUT, ":encoding(console_out)" );
+        binmode( STDERR, ":encoding(console_out)" );
+    }
+    Encode::Locale::decode_argv();
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 ИМЯ
+
+POD2::RU - Perl Документация по-русски
+
+=head1 СИНТАКСИС
+
+  $ perldoc POD2::RU::<название_pod>
+
+  $ perl -MPOD2::RU -e print_pods
+  $ perl -MPOD2::RU -e print_pod <название_pod1> [<название_pod2> ...]
+
+  use POD2::RU;
+  print_pods();
+  print_pod('pod_foo', 'pod_baz');
+
+  use POD2::RU;
+  my $pod2 = POD2::RU->new();
+  $pod2->print_pods();
+  $pod2->print_pod('perlfunc');
+                                                                                          
+
+=head1 ОПИСАНИЕ
+
+Этот модуль содержит перевод Perl-документации на русский. Домашняя страница проекта ─ L<https://github.com/mishin/perldoc-ru>. 
+
+После установки пакета, вы можете использовать следующую команду, чтобы получить документацию:
+
+  $ perldoc POD2::RU::<название_pod>
+
+Начиная с Pod::Perldoc версии 3.14 можно использовать следующий синтаксис:
+
+  $ perldoc -L RU <название_pod>
+  $ perldoc -L RU -f <функция>
+  $ perldoc -L RU -q <регулярное выражение для FAQ>
+
+Модификатор  C<-L> определяет код языка перевода. Если пакет C<< POD2::<код_языка> >> не существует, то модификатор игнорируется.
+
+Для ленивых можно добавить псевдоним (alias):
+
+  perldoc-ru="perldoc -L RU"
+
+для того, чтобы не писать модификатор  C<-L> каждый раз:
+
+  $ perldoc-ru perlre
+  $ perldoc-ru -f map
+
+Начиная с  F<Pod::Perldoc> версии 3.15 вы можете использовать переменную среды PERLDOC_POD2. Если эта переменная установлена в '1', то perldoc осуществляет поиск pod документации на языке, указанном в переменной LC_ALL, LANG или LC_LANG. Или же вы можете установить значение "ru", означающее вывод документации на русском языке. Например,
+
+       export PERLDOC_POD2="ru"
+       perldoc perl
+
+=head1 API
+
+Пакет экспортирует следующие функции:
+
+=over 4
+
+=item * C<new>
+
+Была добавлена для совместимости с C<perldoc> Perl 5.10.1.
+L<Pod::Perldoc> использует ее для возврата имени пакета перевода.
+
+=item * C<pod_dirs>
+
+Была добавлена для совместимости с C<perldoc> Perl 5.10.1.
+L<Pod::Perldoc> использует ее для определения, где искать переведенные файлы.
+
+=item * C<print_pods>
+
+Выводит все переведенные части и оригинальную версию Perl.
+
+=item * C<print_pod>
+
+Выводит на экран оригинальнуя версию Perl для всех частей, переданых в качестве аргументов.
+
+=item * C<search_perlfunc_re>
+
+C<< perldoc -f function >> использует возвращаемое значение этой функции (как regexp), чтобы пропустить введение и найти список функций.
+
+=back
+
+=head1 АВТОРЫ
+
+=over
+
+=item * Николай Мишин  C<< <mishin@cpan.org> >>
+=item * Анатолий Шарифулин  C<< <sharifulin@gmail.com> >>
+=item * Алексей Суриков C<< <KSURi> >>
+=item * Михаил Любимов  C<< <mikhail.lyubimov> >>
+=item * Дмитрий Константинов  C<< <Dim_K> >>
+=item * Евгений Баранов  C<< <Baranov> >>
+=item * Антон Николаев C<< <me@lelf.lu> >>
+=item * Динар Жамалиев C<< <zhdinar> >>
+=item * Валерий Кравчук C<< <valera@openxs.kiev.ua> >>
+
+=back
+
+=head1 ПРОЕКТ
+
+Домашняя страница L<https://github.com/mishin/perldoc-ru>.
+
+
+=head1 СМОТРИТЕ ТАКЖЕ
+
+L<POD2::ES>, L<POD2::PT_BR>, L<POD2::IT>, L<POD2::FR>, L<POD2::LT>.
+
+
+=head1 ПОЖЕРТВОВАНИЕ
+
+Проект перевода документации Perl требует постоянных усилий, что доступно только для сильных духов и альтруистов.
+Авторы не просят, но ни не отвергают помощь в виде денег, книг, сыра и продуктов из свинины (можно хамон;) ), или даже поездки в Полинезию для снижения утомления и сохранения боевого духа команды. Мы приглашаем всех.
+
+
+
+=head1 ОШИБКИ
+
+Информацию об ошибках и пожелания можно оставить на странице
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=POD2-RU> или
+отправить на e-mail C<bug-pod2-ru at rt.cpan.org>, C<mi at ya.ru>.
+
+=head1 ПОДДЕРЖКА
+
+Для просмотра документации в этом модуле, используйте команду C<perldoc>.
+
+    perldoc POD2::RU
+
+
+Вы также можете найти информацию о:
+
+=over 4
+
+=item * RT: системы отслеживания ощибок для CPAN
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=POD2-RU>
+
+=item * AnnoCPAN: аннотированная документация CPAN
+
+L<http://annocpan.org/dist/POD2-RU>
+
+=item * Рейтинги CPAN
+
+L<http://cpanratings.perl.org/d/POD2-RU>
+
+=item * Поиск CPAN модулей
+
+L<http://search.cpan.org/dist/POD2-RU/>
+
+=back
+
+
+=head1 БЛАГОДАРНОСТЬ
+
+Авторы хотели бы выразить свою благодарность команде разработчиков OmegaT, инструмента, который используется для перевода.
+
+Проект OmegaT: L<http://omegat.org/>
+
+Также хотелось бы выразить благодарность автору перевода perl документации на испанский язык 
+POD2::ES Enrique Nell за его в помощь в напимании скриптов и вообще основу  взял с этого репозитория.
+ 
+Проект POD2::ES: L<https://github.com/zipf/perldoc-es>
+
+Благодарность Yusuke Kawasaki за совет разместить весь проект на github
+на вопрос о японской документации он ответил:
+They (JPRP people) uses sourceforge and its CVS. 
+
+L<http://sourceforge.jp/cvs/view/perldocjp/docs/modules/>
+
+They are using a traditional mailing list and sourceforge's bug tracking system. In my opinion, github would be a much better way to manage those things at the moment. github was not exist when they started the project.
+
+
+=head1 ЛИЦЕНЗИИ И АВТОРСКИЕ ПРАВА
+
+Copyright (C) 2011-2013 Команда Perl по-русски, Moscow-pm.
+
+Данная программа является свободным программным обеспечением, вы можете распространять и изменять его в соответствии с условиями общественной лицензии GNU GPL, опубликованной Фондом свободного программного обеспечения, или Artistic License.
+
+См. L<http://dev.perl.org/licenses> для более подробной информации.
+
+
+=cut
---
+++ scripts/get_pod_one_liners.md
@@ -0,0 +1,165 @@
+```perl
+perl -Ilib -MIO::All -E"say$_->name.q{ }.$_->getlines for sort {$a->size<=>$b->size}grep{/lre[^fp]/}io->dir((`perldoc -l perl`=~/(.+?)[\w.]+$/))->all"  
+```
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlreguts.pod 886 112/886     +/-  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlre.pod 2585 108 +/-  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlretut.pod 2928 2607  89%    +/-  
+perl -E "say 1/(2928/2606)"
+
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perldebtut.pod 730 130 18%             +/-  
+
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlrebackslash.pod 661 22 +/-  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlrecharclass.pod 851 30 +/-   
+
+view https://github.com/mishin/perldoc-ru/blob/master/pod2-ru/POD2-RU/scripts/get_pod_one_liners.md  
+edit https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/POD2-RU/scripts/get_pod_one_liners.md  
+edit https://github.com/mishin/parse_datastage_dsx/edit/master/Dsx_parse/Tools.pm  
+https://github.com/mishin/perlsecret/edit/master/lib/perlsecret.ru.pod  
+
+https://github.com/mishin/perldoc-ru/blob/master/pod2-ru/target/pods/perlperf.pod  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlunicode.pod 1857 23 +/-  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlreapi.pod 706 57           +/-  
+
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perldebguts.pod 10             +/-  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perldebug.pod 10             +/-  
+
+
+ready
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlreref.pod 408             +  
+https://github.com/mishin/perldoc-ru/edit/master/pod2-ru/target/pods/perlrequick.pod 519           +  
+
+
+C:\Perl64\lib\pods\perlreref.pod 408             +  
+C:\Perl64\lib\pods\perlrequick.pod 519           +  
+C:\Perl64\lib\pods\perlreapi.pod 706 57         +/-  
+C:\Perl64\lib\pods\perlrebackslash.pod 661 131    +/-  
+C:\Perl64\lib\pods\perlrecharclass.pod 850 78    +/-   
+C:\Perl64\lib\pods\perlreguts.pod 886 20/886     +/-  
+C:\Perl64\lib\pods\perlre.pod 2585 107           +/-  
+C:\Perl64\lib\pods\perlretut.pod 2928 512 17%    +-  
+perlsecret.ru.pod 959 959 +  
+C:\Perl64\lib\pods\perlunicode.pod 1857 49 +/-  
+
+perlunicode.pod  
+
+
+```perl
+perl -MIO::All -E'say$_->name.q{ }.$_->getlines for sort {$a->size<=>$b->size}grep{/lre[^f]/}io->dir((`perldoc -l perl`=~/(.+?)[\w.]+$/))->all'
+```,
+/usr/share/perl/5.18/pod/perlrepository.pod 18  
+/usr/share/perl/5.18/pod/perlreref.pod 408       +  
+/usr/share/perl/5.18/pod/perlrequick.pod 519     +  
+/usr/share/perl/5.18/pod/perlrebackslash.pod 664  
+/usr/share/perl/5.18/pod/perlreapi.pod 826  
+/usr/share/perl/5.18/pod/perlreguts.pod 928  
+/usr/share/perl/5.18/pod/perlrecharclass.pod 1081  
+/usr/share/perl/5.18/pod/perlre.pod 2614         +-  
+/usr/share/perl/5.18/pod/perlretut.pod 2928 512 17%   +-  
+
+
+
+```perl
+perl -MIO::All -E"say$_->name.q{ }.$_->getlines for sort {$a->size<=>$b->size}grep{/lre[^f]/}io->dir((`perldoc -l perl`=~/(.+?)[\w.]+$/))->all"
+```
+/usr/share/perl/5.18/pod/perlrepository.pod 18  
+/usr/share/perl/5.18/pod/perlreref.pod 408   +  
+/usr/share/perl/5.18/pod/perlrequick.pod 519 +   
+/usr/share/perl/5.18/pod/perlreftut.pod 526  
+/usr/share/perl/5.18/pod/perlrebackslash.pod 664  
+/usr/share/perl/5.18/pod/perlref.pod 754  
+/usr/share/perl/5.18/pod/perlreapi.pod 826  
+/usr/share/perl/5.18/pod/perlreguts.pod 928  
+/usr/share/perl/5.18/pod/perlrecharclass.pod 1081  
+/usr/share/perl/5.18/pod/perlre.pod 2614  
+/usr/share/perl/5.18/pod/perlretut.pod 2928  
+
+```perl
+my @lines = map {chomp; $_} $io->slurp;
+
+
+perl -MIO::All -E "$p{$_->name}=map{$l++}$_->slurp for grep{/perlre/}io('.')->all;for $k(sort {$p{$a}<=>$p{$b}} keys %p){say qq{$k $p{$k}}}"
+
+perl -MIO::All -E "$p{$_->name}=map{chomp}$_->slurp for grep{/perlre/}io('.')->all;for $k(sort {$p{$a}<=>$p{$b}} keys %p){say qq{$k $p{$k}}}"
+
+perl -I"c:\Users\TOSH\Documents\GitHub\io-all-mishin\lib" -MIO::All -e "printf qq{%s %s\n},$_->name,$_->rows for sort {$a->rows<=>$b->rows}grep{$_->name=~/perlre.*\.pod/}io(q(.))->all"
+
+perl -MIO::All -E "$p{$_->name}=map{$l++}$_->slurp for grep{/perlre/}io('.')->all;for $k(sort {$p{$a}<=>$p{$b}} keys %p){say qq{$k $p{$k}}}"
+
+
+cd c:\Users\TOSH\Documents\GitHub\perldoc-ru\pod2-ru\target\pods
+set path_2_lib=c:\Users\TOSH\Documents\GitHub\io-all-mishin\lib
+perl  -I%path_2_lib% -MIO::All -e "printf qq{%s %s\n},$_->name,$_->rows for sort{$a->rows<=>$b->rows}grep{/perlre/}io(q(.))->all"
+
+```
+
+
+http://habrahabr.ru/company/luxoft/blog/215345/
+
+```
+git filter-branch --env-filter \
+    'if [ $GIT_COMMIT = 119f9ecf58069b265ab22f1f97d2b648faf932e0 ]
+     then
+         export GIT_AUTHOR_DATE="Fri Jan 2 21:38:53 2009 -0800"
+         export GIT_COMMITTER_DATE="Sat May 19 01:01:01 2007 -0700"
+     fi'
+	 
+git commit --amend --date="Wed Feb 16 14:00 2011 +0100"
+GIT_COMMITTER_DATE="Wed Feb 16 14:00 2011 +0100" git commit --amend
+```
+Here is a convenient alias that changes both commit and author times of the last commit to a time accepted by date --date:
+
+```
+[alias]
+    cd = "!d=\"$(date -d \"$1\")\" && shift && GIT_COMMITTER_DATE=\"$d\" \
+            git commit --amend --date \"$d\""
+Usage: git cd <date_arg>
+```
+Examples:
+```
+git cd now  # update the last commit time to current time
+git cd '1 hour ago'  # set time to 1 hour ago
+```
+Edit: Here is a more-automated version which checks that the index is clean (no uncommitted changes) and reuses the last commit message, or fails otherwise (fool-proof):
+```
+[alias]
+    cd = "!d=\"$(date -d \"$1\")\" && shift && \
+        git diff-index --cached --quiet HEAD --ignore-submodules -- && \
+        GIT_COMMITTER_DATE=\"$d\" git commit --amend -C HEAD --date \"$d\"" \
+        || echo >&2 "error: date change failed: index not clean!"	 
+```		
+The following bash function will change the time of any commit on the current branch.
+
+Be careful not to use if you already pushed the commit or if you use the commit in another branch.
+```
+# rewrite_commit_date(commit, date_timestamp)
+#
+# !! Commit has to be on the current branch, and only on the current branch !!
+# 
+# Usage example:
+#
+# 1. Set commit 0c935403 date to now:
+#
+#   rewrite_commit_date 0c935403
+#
+# 2. Set commit 0c935403 date to 1402221655:
+#
+#   rewrite_commit_date 0c935403 1402221655
+#
+rewrite_commit_date () {
+    local commit="$1" date_timestamp="$2"
+    local date temp_branch="temp-rebasing-branch"
+    local current_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+    if [[ -z "$commit" ]]; then
+        date="$(date -R)"
+    else
+        date="$(date -R --date "@$date_timestamp")"
+    fi
+
+    git checkout -b "$temp_branch" "$commit"
+    GIT_COMMITTER_DATE="$date" git commit --amend --date "$date"
+    git checkout "$current_branch"
+    git rebase "$commit" --onto "$temp_branch"
+    git branch -d "$temp_branch"
+}		
+```
